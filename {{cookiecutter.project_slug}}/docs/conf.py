72eeaefc2422c90d878b521238f502bbfd8284c5
cbows <32486983+cbows@users.noreply.github.com>
2020-01-25 20:01:04 +0100

    Add extension.
    Explicitly add sphinx-rtd-theme extension.

--- {{cookiecutter.project_slug}}/docs/conf.py
+++ {{cookiecutter.project_slug}}/docs/conf.py
@@ -34,7 +34,7 @@ version = {{ cookiecutter.project_slug }}.__version__
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx_rtd_theme"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
